d6caec6b357a23e778908e7f7e1829ea13174343
jpire <he201388@students.ephec.be>
2018-12-24 02:09:13 +0100

    setup symfony project : security with jwt

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use iHasRole;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
+    public function load(ObjectManager $manager)
+    {
+        $userTest = new User();
+        $userTest->setUsername('test');
+        $userTest->setEmail('test@gmail.com');
+        $birthDate = new DateTime();
+        $userTest->setBirthDate($birthDate);
+        $userTestRoles = array(iHasRole::ROLE_USER);
+        $userTest->setRoles($userTestRoles);
+        $userTest->setPassword($this->passwordEncoder->encodePassword(
+            $userTest,
+            'test'
+        ));
+
+        $manager->persist($userTest);
+        $manager->flush();
+    }
+}
---
+++ src/Security/Guard/TokenAuthenticator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpire
+ * Date: 23/12/18
+ * Time: 2:46
+ */
+
+namespace App\Security\Guard;
+
+
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator as BaseAuthenticator;
+
+class TokenAuthenticator extends BaseAuthenticator
+{
+
+}
+
