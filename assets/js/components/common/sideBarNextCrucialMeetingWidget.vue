1751725ba9bccc3fbcf5a845ec712a8b94af9307
PireJoseph <he201388@students.ephec.be>
2020-07-08 20:58:46 +0200

    fix(menuWidget): fix a bug when there is no meeting upcoming

--- assets/js/components/common/sideBarNextCrucialMeetingWidget.vue
+++ assets/js/components/common/sideBarNextCrucialMeetingWidget.vue
@@ -16,7 +16,7 @@
 
 <template>
 
-    <div v-if="Object.keys(nextCrucialMeeting).length" class="w3-card w3-round w3-white w3-center">
+    <div v-if="!!nextCrucialMeeting && Object.keys(nextCrucialMeeting).length" class="w3-card w3-round w3-white w3-center">
         <div class="w3-container">
             <h6><strong>Prochaine rencontre importante</strong></h6>
             <p id="sideBarNextCrucialMeetingLabel" >{{ nextCrucialMeeting.label }}</p>
